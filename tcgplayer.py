b5559c1e75d2c2789704fd62c88ddca60004441a
Andy Ward <andrew.ward.cs@gmail.com>
2015-08-28 16:14:07 -0400

    Modified apis for deck loaders, both now use the same deck class

--- tcgplayer.py
+++ tcgplayer.py
@@ -4,95 +4,96 @@ import ygocard
 import devpro
 import re
 		
-class TCGDeck(ygocard.YugiohDeck):
-	@staticmethod
-	def open(print_url):
-		conn = urllib2.urlopen(print_url)
-		html = conn.read()
-		conn.close()
-		
-		dom = bsoup(html, 'lxml')
-		
-		heading = dom.div.b
-		assert(heading != None)
-		
-		name = str(heading.contents[0].strip())
-		author = str(heading.contents[2].strip())
-
-		main = []
-		main_deck = dom.find('b', string='Main Deck:').find_next_sibling('font')
-		for line in main_deck.children:
-			if line.name != 'br':
-				data = str(line.string.strip())
-				if data != '':
-					main.append(data)
-
-		side = []
-		extra = []
-		current = side
-		
-		side_deck = dom.find('b', string='Sideboard:').find_next_sibling('font')
-		for line in side_deck.children:
-			if line.name == 'b' and line.string == 'Extra Deck:':
-				current = extra
-			elif line.name != 'br':
-				data = str(line.string.strip())
-				if data != '':
-					current.append(data)
-		return TCGDeck.from_scrape(name, author, main, side, extra)
-	@staticmethod
-	def from_scrape(name, author, mdeck, sdeck, edeck):
-		db = devpro.database()
-		main = []
-		for line in mdeck:
-			count = int(line[0])
-			text = line[2:]
-			cards = list(db.cards_by_pattern(text))
-			if len(cards) > 0:
-				card = cards[0]
-				for i in range(count):
-					main.append(card)
-			else:
-				raise RuntimeError('Could not find card with name "{0}"'.format(text))
-
-		side = []
-		for line in sdeck:
-			count = int(line[0])
-			text = line[2:]
-			cards = list(db.cards_by_pattern(text))
-			if len(cards) > 0:
-				card = cards[0]
-				for i in range(count):
-					side.append(card)
-			else:
-				raise RuntimeError('Could not find card with name "{0}"'.format(text))
-	
-		extra = []
-		for line in edeck:
-			count = int(line[0])
-			text = line[2:]
-			cards = list(db.cards_by_pattern(text))
-			if len(cards) > 0:
-				card = cards[0]
-				for i in range(count):
-					extra.append(card)
-			else:
-				raise RuntimeError('Could not find card with name "{0}"'.format(text))
-
-		return TCGDeck(name, author, main, side, extra)
-
-
-# api stuff
-
-def tcg_open(arg):
+PRINTABLE_URL_TEMPLATE="http://yugioh.tcgplayer.com/db/deck_print.asp?deck_id="
+STANDARD_URL_TEMPLATE="http://yugioh.tcgplayer.com/db/deck.asp?deck_id="
+def tcg_open(print_url):
+	# supports opening two kinds of url, as well as raw deck id.
 	if isinstance(arg, int):
-		url = "http://yugioh.tcgplayer.com/db/deck_print.asp?deck_id="+str(arg)
-	elif arg.startswith('http://yugioh.tcgplayer.com/db/deck.asp'):
+		url = PRINTABLE_URL_TEMPLATE+str(arg)
+	elif arg.startswith(STANDARD_URL_TEMPLATE):
 		match = re.search('\?deck_id=([0-9]+)', arg)
 		if match == None:
 			raise RuntimeError('URL does not point to a yugioh.TCGPlayer.com decklist')
 		else:
-			url = "http://yugioh.tcgplayer.com/db/deck_print.asp?deck_id=" + match.group(1)
-	elif arg.startswith('http://yugioh.tcgplayer.com/db/deck_print.asp'):
+			url = PRINTABLE_URL_TEMPLATE + match.group(1)
+	elif arg.startswith(PRINTABLE_URL_TEMPLATE):
 		url = arg
-	return TCGDeck.open(url)
+
+	conn = urllib2.urlopen(url)
+	html = conn.read()
+	conn.close()
+	
+	dom = bsoup(html, 'lxml')
+	
+	heading = dom.div.b
+	assert(heading != None)
+	
+	name = str(heading.contents[0].strip())
+	author = str(heading.contents[2].strip())
+
+	main = []
+	main_deck = dom.find('b', string='Main Deck:').find_next_sibling('font')
+	for line in main_deck.children:
+		if line.name != 'br':
+			data = str(line.string.strip())
+			if data != '':
+				main.append(data)
+
+	side = []
+	extra = []
+	current = side
+	
+	side_deck = dom.find('b', string='Sideboard:').find_next_sibling('font')
+	for line in side_deck.children:
+		if line.name == 'b' and line.string == 'Extra Deck:':
+			current = extra
+		elif line.name != 'br':
+			data = str(line.string.strip())
+			if data != '':
+				current.append(data)
+	return __from_scrape(name, author, main, side, extra)
+
+def __from_scrape(name, author, mdeck, sdeck, edeck):
+	# converts raw card names from the scrape to actual YugiohCard instances.
+	# uses devpro backend.
+	db = devpro.database()
+	main = []
+	for line in mdeck:
+		count = int(line[0])
+		text = line[2:]
+		cards = list(db.cards_by_pattern(text))
+		if len(cards) > 0:
+			card = cards[0]
+			for i in range(count):
+				main.append(card)
+		else:
+			raise RuntimeError('Could not find card with name "{0}"'.format(text))
+
+	side = []
+	for line in sdeck:
+		count = int(line[0])
+		text = line[2:]
+		cards = list(db.cards_by_pattern(text))
+		if len(cards) > 0:
+			card = cards[0]
+			for i in range(count):
+				side.append(card)
+		else:
+			raise RuntimeError('Could not find card with name "{0}"'.format(text))
+
+	extra = []
+	for line in edeck:
+		count = int(line[0])
+		text = line[2:]
+		cards = list(db.cards_by_pattern(text))
+		if len(cards) > 0:
+			card = cards[0]
+			for i in range(count):
+				extra.append(card)
+		else:
+			raise RuntimeError('Could not find card with name "{0}"'.format(text))
+	return ygocard.YugiohDeck(name, author, main, side, extra)
+
+def tcg_save(deck, fl):
+	# this is just here for symettry
+	raise NotImplementedError('TCGPlayer does not support uploading decks.')
--- ydk.py
+++ ydk.py
@@ -4,51 +4,42 @@ import collections
 import os
 import devpro
 
-class YDK(ygocard.YugiohDeck):
-	@staticmethod	
-	def open(ydk_path):
-		name = os.path.basename(ydk_path)[:-4]
-		db = devpro.database()
-		main = []
-		side = []
-		extra = []
-		author = ''
-		current = main
-		db.open()
-		with open(ydk_path) as fl:
-			for line in fl:
-				if line.startswith('#created by '):
-					author = (line.rstrip())[11:]
-				elif line.startswith('#main'):
-					current = main
-				elif line.startswith('#extra'):
-					current = extra
-				elif line.startswith('!side'):
-					current = side
-				else:
-					cid = int(line.rstrip())
-					card = db.card_by_id(cid)
-					current.append(card)
-		db.close()
-		return YDK(name, author, main, side, extra)
-		
-	def save(self):
-		output = '#created by {0}\n'.format(self.author)
-		output += '#main\n'
-		for card in self.main():
-			output += '{0}\n'.format(card.cid())
-		output += '#extra\n'
-		for card in self.extra():
-			output += '{0}\n'.format(card.cid())
-		output += '!side\n'
-		for card in self.side():
-			output += '{0}\n'.format(card.cid())
-		
-
-	
-
-# api stuff
+def ydk_save(deck, fl):
+	output = '#created by {0}\n'.format(deck.author)
+	output += '#main\n'
+	for card in deck.main():
+		output += '{0}\n'.format(card.cid())
+	output += '#extra\n'
+	for card in deck.extra():
+		output += '{0}\n'.format(card.cid())
+	output += '!side\n'
+	for card in deck.side():
+		output += '{0}\n'.format(card.cid())
+	fl.write(output)
 
 def ydk_open(path):
-	return YDK.open(path)
+	name = os.path.basename(path)[:-4]
+	db = devpro.database()
+	main = []
+	side = []
+	extra = []
+	author = ''
+	current = main
+	db.open()
+	with open(path) as fl:
+		for line in fl:
+			if line.startswith('#created by '):
+				author = (line.rstrip())[11:]
+			elif line.startswith('#main'):
+				current = main
+			elif line.startswith('#extra'):
+				current = extra
+			elif line.startswith('!side'):
+				current = side
+			else:
+				cid = int(line.rstrip())
+				card = db.card_by_id(cid)
+				current.append(card)
+	db.close()
+	return ygocard.YugiohDeck(name, author, main, side, extra)
 
